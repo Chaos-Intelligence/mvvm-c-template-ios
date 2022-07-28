8b391b7af31b5e2a3f95f19de463338e252bdb63
Glenn Posadas <hello@glennvon.com>
2022-07-28 17:57:09 +0800

    add routing

--- MVVM-Coordinator-Store.xctemplate/___FILEBASENAME___Coordinator.swift
+++ MVVM-Coordinator-Store.xctemplate/___FILEBASENAME___Coordinator.swift
@@ -1,16 +1,24 @@
 import Combine
 import UIKit
 
+enum ___VARIABLE_productName___Route {
+  // case showSceneA
+}
+
 class ___FILEBASENAMEASIDENTIFIER___: NSObject, Coordinator {
   
-  // MARK: - 
+  // MARK: -
   // MARK: Properties
   
   var childCoordinators = [Coordinator]()
   var navigationController: BaseNavigationController
   var cancellables = Set<AnyCancellable>()
   
-  // MARK: - 
+  var route = PassthroughSubject<___VARIABLE_productName___Route, Never>()
+  
+  // private var sceneACoordinator: SceneACoordinator?
+  
+  // MARK: -
   // MARK: Functions
   
   // MARK: Initialization
@@ -22,5 +30,25 @@ class ___FILEBASENAMEASIDENTIFIER___: NSObject, Coordinator {
   func start() {
     let controller = ___VARIABLE_productName___ViewController(viewModel: .init(coordinator: self))
     navigationController.setViewControllers([controller], animated: true)
+    
+    route
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] route in
+        /*switch route {
+        case .showSceneA:
+          self?.showSceneA()
+        }*/
+      }
+      .store(in: &cancellables)
   }
+}
+
+// MARK: -
+// MARK: Navigations
+
+private extension ___FILEBASENAMEASIDENTIFIER___ {
+  /*func showSceneA() {
+    sceneACoordinator = SceneACoordinator()
+    sceneACoordinator?.start()
+  }*/
 }
